730d12f6feb2826d4f69910615acd1717040babe
Metin Yıldıran <metin.1464@gmail.com>
2020-04-21 17:29:31 +0300

    Finished.

---
+++ app/src/main/java/com/example/broadcastreceiverpermissionapp2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.broadcastreceiverpermissionapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signal(View view) {
+        Intent intent = new Intent("my.signal");
+        sendBroadcast(intent, "my.permission");
+    }
+}
